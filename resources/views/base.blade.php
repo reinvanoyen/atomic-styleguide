fd6114303f002ae54bb9d51f4d09e97ebb3096d6
vagrant <vagrant@homestead>
2019-09-06 13:38:36 +0000

    styling progress

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -10,17 +10,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="{{ asset('vendor/atomic-styleguide/css/styleguide.css') }}" rel="stylesheet" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
-
+    <link href="https://fonts.googleapis.com/css?family=Maven+Pro&display=swap" rel="stylesheet" />
 </head>
 <body>
-
-    <div class="styleguide">
-        <div class="styleguide-title">{{ config('styleguide.name')  }}</div>
-        <div class="styleguide-content">
+    <div class="asg-styleguide">
+        <div class="asg-styleguide__header">
+            <h1 class="asg-title">
+                {{ config('styleguide.name') }}
+            </h1>
+        </div>
+        <div class="asg-styleguide__content">
             @yield('body')
         </div>
     </div>
     <script src="{{ asset('js/app.js')  }}"></script>
-
 </body>
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,20 +2,19 @@
 
 @section('body')
 
-    <div class="styleguide-type-list">
-
+    <div class="asg-component-list">
         @foreach($types as $type)
-            <div class="styleguide-type">
-                <div class="styleguide-type__title">
-                    <a href="{{ route('styleguide.type', ['type' => $type['slug'],]) }}" title="{{ $type['name'] }}">
+            <div class="asg-component-list__item">
+                <div class="asg-component-list__header">
+                    <a class="asg-title" href="{{ route('styleguide.type', ['type' => $type['slug'],]) }}" title="{{ $type['name'] }}">
                         {{ $type['name'] }} ({{  count($type['components']) }})
                     </a>
                 </div>
-                <div class="styleguide-type__content">
+                <div class="asg-component-list__content">
                     <ul>
                         @foreach($type['components'] as $component)
                             <li>
-                                <a href="{{ route('styleguide.component', ['type' => $type['slug'], 'component' => $component['name']]) }}" title="{{ $component['name'] }}">
+                                <a class="asg-tag" href="{{ route('styleguide.component', ['type' => $type['slug'], 'component' => $component['name']]) }}" title="{{ $component['name'] }}">
                                     {{ $component['name'] }}
                                 </a>
                             </li>
@@ -24,7 +23,5 @@
                 </div>
             </div>
         @endforeach
-
     </div>
-
 @endsection
